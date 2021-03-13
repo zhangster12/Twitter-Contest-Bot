d3c83019aa694f73868636cb08c9ee4ceec7a4c4
Daniel Zhang <zhangster12@gmail.com>
2021-03-13 01:28:06 -0500

    Edit existing file

--- twitterUnfollow.py
+++ twitterUnfollow.py
@@ -1,7 +1,6 @@
-# Unfollows users if the authenticating user's following number is greater than 2000.
-# Will unfollow users until the authenticating user's following number is 1500.
+# Randomly unfollows users if the authenticating user's following number is greater than 2000 until it's 1500
 
-import os, time, tweepy
+import os, random, time, tweepy
 from tweepy.cursor import Cursor
 from auth import api
 
@@ -12,11 +11,23 @@ my_screen_name = api.me().screen_name
 
 if following_count > 2000:
     print(f'Current following count is at {following_count}.')
-    for friend in tweepy.Cursor(api.friends).items(following_count - 1500): 
+    for friend in tweepy.Cursor(api.friends).items(following_count - 1000):
+        
         # Checks if user is following authenticating user
         status = api.show_friendship(source_screen_name = friend.screen_name, target_screen_name = my_screen_name)
         if status[0].following:
+            print(f'{friend.screen_name} follows {my_screen_name}.')
             continue
+        
+        # Randomly skips users
+        elif random.randint(0, 1) == 1:
+            print('User has been skipped.')
+            continue
+        
+        # Ends loop if following count is 1500
+        elif api.me().friends_count == 1500:
+            break
+        
         else:
             api.destroy_friendship(friend.screen_name)
             print(f'{friend.screen_name} has been unfollowed.')
