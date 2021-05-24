76c447cd9f3c8310ab8f2c5ec76756d96378b229
zhangster12 <70448334+zhangster12@users.noreply.github.com>
2021-05-23 21:08:52 -0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'Bi5Zzb6nl41grnJ1Xn1rxo4PY'
-consumer_secret = 'cH2oLaZ67w44p1EIjsrp7uWXA0J6v9YHHMgGJKadkQBH35suBL'
-access_token = '1198257732761128960-fPNjttTmjWEqVeHpiuw7I9uuCFCbtH'
-access_token_secret = 'PsYjwInTECmlLQFc5SuIhyAdfS7WoX5dZoUZhcS25UHNl'
+consumer_key = 'consumer_key'
+consumer_secret = 'consumer_secret'
+access_token = 'access_token'
+access_token_secret = 'access_token_secret'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
